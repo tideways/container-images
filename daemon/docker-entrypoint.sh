e8637ec98414465e8723e57df56f55fdf33c7925
Tim Düsterhus <tim@tideways-gmbh.com>
2024-03-13 16:26:52 +0100

    Initial import of daemon Dockerfile

---
+++ daemon/docker-entrypoint.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+if [ "$#" -eq 1 ] && [ "$1" = "tideways-daemon" ]; then
+	TIDEWAYS_DAEMON_EXTRA=
+
+	if [ -f /etc/default/tideways-daemon ]; then
+		source /etc/default/tideways-daemon
+	fi
+
+	hostname=
+	# Set a readable hostname if the hostname looks like Docker's
+	# default hostname derived from the container ID.
+	if [[ "$(hostname)" =~ ^[0-9a-f]{12}$ ]]; then
+		hostname=--hostname=tideways-daemon
+	fi
+
+	set -- tideways-daemon --address="[::]:9135" $hostname $TIDEWAYS_DAEMON_EXTRA
+fi
+
+exec "$@"
